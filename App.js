07e2c85f26c8e90799a8f1f331f0d711ed203dcc
rudranilbanerjee <rudranilbanerjee192@gmail.com>
2026-08-30 11:53:04 +0530

    Push JSX concept code.

--- App.js
+++ App.js
@@ -1,48 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const main = React.createElement('div',
-//     {
-//         id:"parent",
-//     },
-//     [React.createElement("div",{id:"child"},
-//         [React.createElement("h1",
-//             {
 
-//             },
-//             "I am H1 Tag"
-//         ),
-//         React.createElement("h2",
-//             {
+// React.createElement ==> Js Object => HTML Element
+const heading = React.createElement("h1",{id:"heading"},"Hello From React");
+console.log("heading",heading);
 
-//             },
-//             "I am H2 Tag"
-//         )]
+// JSX => Javascript XML
+// its look like a html or xml but its not html or xml its a javascript object
+const jsxHeading = <h1 id="heading" tabIndex="1" className="abcd">
+    <span>Hello From JSX</span>
+<p>Hello From Paragraph</p>
+</h1>;
+// Parcel==> babel==> React.createElement ==> JS Object => HTML Element
+console.log("jsxHeading",jsxHeading);
 
-//     ),
-//     React.createElement("div",{id:"child"},
-//         [React.createElement("h1",
-//             {
 
-//             },
-//             "I am H1 Tag"
-//         ),React.createElement("h2",
-//             {
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-//             },
-//             "I am H2 Tag"
-//         )]
 
-//     )]
-// );
-
-//JSX
-const main = React.createElement(
-    "div",
-    {id:"parent"},
-    "Hi i am React")
-console.log(main)
-const root = ReactDOM.createRoot(
-    document.getElementById('root')
-);
-root.render(main);  
+root.render(jsxHeading);
