2c85ee353540301c5be0bc88db889e99e06971a6
rudranilbanerjee <rudranilbanerjee192@gmail.com>
2026-08-30 12:47:40 +0530

    Today lession 30.08.2026

--- App.js
+++ App.js
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 
+//Component in react
 
-// React.createElement ==> Js Object => HTML Element
-const heading = React.createElement("h1",{id:"heading"},"Hello From React");
-console.log("heading",heading);
+// Types of component
+// 1) Class based Component
+// 2) Functional Component
 
-// JSX => Javascript XML
-// its look like a html or xml but its not html or xml its a javascript object
-const jsxHeading = <h1 id="heading" tabIndex="1" className="abcd">
-    <span>Hello From JSX</span>
-<p>Hello From Paragraph</p>
-</h1>;
-// Parcel==> babel==> React.createElement ==> JS Object => HTML Element
-console.log("jsxHeading",jsxHeading);
+
+// const Heading = ()=> <h1>Hello, Start your day</h1>;// react component
+// inside react component i will render my react Element.
+// render react component inside react Element
+// const title = <span><Heading/></span>// react element
+
+// when we put any elememnt inside a component then this process is called component composition.
+
+// const data =<script>alert("hacked")</script>;// some details of user // but after hack i got this 
+
+
+// JSX Auto escaping
+// const Heading = ()=> <h1>{data}</h1>
+
+// React component open and close tag
+
+const Heading = ()=> <h1>Hello, Start your day</h1>;
+
+const title = <span><Heading/></span>
+const title1 = <span><Heading></Heading></span>
+// there component called as open and closing tag
+const title2 = <span>{Heading()}</span>
+// there component called as a function invokation
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
-root.render(jsxHeading);
+root.render(title2);
